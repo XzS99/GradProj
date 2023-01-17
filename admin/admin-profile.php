e4f24e2ce14e3c5720477079739c8d4a4a30ec99
wesam jarrah <wesam.jarrahh@gmail.com>
2023-01-18 01:14:14 +0300

    1.2 admin profile footer fixed

--- admin/admin-profile.php
+++ admin/admin-profile.php
@@ -1,58 +1,63 @@
 <?php
 session_start();
-include('includes/dbconnection.php');
-if (strlen($_SESSION['lssemsaid']) == 0) {
+include 'includes/dbconnection.php';
+
+// Check if user is logged in
+if (empty($_SESSION['lssemsaid'])) {
     header('location:logout.php');
-} else {
-    if (isset($_POST['submit'])) {
-        $adminid = $_SESSION['lssemsaid'];
+    exit;
+}
 
-        // Extract the values from the POST request and assign them to variables 
-        $AName = $_POST['adminname'];
-        $mobno = $_POST['mobilenumber'];
-        $email = $_POST['email'];
+// Check if form was submitted
+if (isset($_POST['submit'])) {
+    $admin_id = $_SESSION['lssemsaid'];
 
-        // Update the tbladmin table with the new values 
-        $sql = "UPDATE tbladmin SET AdminName=:adminname, MobileNumber=:mobilenumber, Email=:email WHERE ID=:aid";
+    // Extract form data
+    $admin_name = filter_input(INPUT_POST, 'adminname', FILTER_SANITIZE_STRING);
+    $mobile_number = filter_input(INPUT_POST, 'mobilenumber', FILTER_SANITIZE_STRING);
+    $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
 
-        // Prepare and execute the query 
+    if ($admin_name && $mobile_number && $email) {
+        // Update the tbladmin table with the new values
+        $sql = "UPDATE tbladmin SET AdminName=:adminname, MobileNumber=:mobilenumber, Email=:email WHERE ID=:admin_id";
+
+        // Prepare and execute the query
         $query = $dbh->prepare($sql);
-
-        // Bind parameters to their respective values 
-        $query->bindParam(':adminname', $AName, PDO::PARAM_STR);
+        $query->bindParam(':adminname', $admin_name, PDO::PARAM_STR);
         $query->bindParam(':email', $email, PDO::PARAM_STR);
-        $query->bindParam(':mobilenumber', $mobno, PDO::PARAM_STR);
-        $query->bindParam(':aid', $adminid, PDO::PARAM_STR);
+        $query->bindParam(':mobilenumber', $mobile_number, PDO::PARAM_STR);
+        $query->bindParam(':admin_id', $admin_id, PDO::PARAM_STR);
 
-        // Execute the query and check for success/failure of execution  
+        // Execute the query and check for success/failure of execution
         if ($query->execute()) {
-
-            // Display an alert message on successful update  
+            // Display an alert message on successful update
             echo '<script>alert("Your profile has been updated")</script>';
         } else {
-
-            // Display an alert message on unsuccessful update  
+            // Display an alert message on unsuccessful update
             echo '<script>alert("Unable to update profile")</script>';
         }
+    } else {
+        echo '<script>alert("Invalid data provided")</script>';
     }
 }
+
+// HTML Markup
 ?>
 
 <!DOCTYPE html>
 <html>
 
 <head>
-
     <title>Local Services Search Engine Mgmt System | Admin Profile</title>
 
     <!-- Font Awesome -->
     <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
     <!-- Ionicons -->
     <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-    <!-- Theme style -->
-    <link rel="stylesheet" href="dist/css/adminlte.min.css">
     <!-- Google Font: Source Sans Pro -->
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="dist/css/adminlte.min.css">
 </head>
 
 <body class="hold-transition sidebar-mini">
@@ -97,63 +102,72 @@ if (strlen($_SESSION['lssemsaid']) == 0) {
                                     <div class="card-body card-block">
                                         <?php
 
-                                        $sql = "SELECT * from  tbladmin";
+                                        $sql = "SELECT * from  tbladmin WHERE ID = :adminid";
                                         $query = $dbh->prepare($sql);
+                                        $query->bindValue(':adminid', $_SESSION['lssemsaid'], PDO::PARAM_INT);
                                         $query->execute();
                                         $results = $query->fetchAll(PDO::FETCH_OBJ);
-                                        $cnt = 1;
-                                        if ($query->rowCount() > 0) {
-                                            foreach ($results
 
-                                                as $row) { ?>
-                                                <div class="form-group"><label for="company" class=" form-control-label">Admin
-                                                        Name</label><input type="text" name="adminname" value="<?php echo $row->AdminName; ?>" class="form-control" required='true'></div>
-                                                <div class="form-group"><label for="vat" class=" form-control-label">User
-                                                        Name</label><input type="text" name="username" value="<?php echo $row->UserName; ?>" class="form-control" readonly=""></div>
-                                                <div class="form-group"><label for="street" class=" form-control-label">Contact
-                                                        Number</label><input type="text" name="mobilenumber" value="<?php echo $row->MobileNumber; ?>" class="form-control" maxlength='10' required='true'></div>
+                                        if ($query->rowCount() > 0) {
+                                            foreach ($results as $row) { ?>
+
+                                                <div class="form-group">
+                                                    <label for="company" class="form-control-label">Admin Name</label>
+                                                    <input type="text" name="adminname" value="<?php echo $row->AdminName; ?>" class="form-control" required='true'>
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="vat" class="form-control-label">User Name</label>
+                                                    <input type="text" name="username" value="<?php echo $row->UserName; ?>" class="form-control" readonly="">
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="street" class="form-control-label">Contact Number</label>
+                                                    <input type="text" name="mobilenumber" value="<?php echo $row->MobileNumber; ?>" class="form-control" maxlength='10' required='true'>
+                                                </div>
                                                 <div class="row form-group">
                                                     <div class="col-12">
-                                                        <div class="form-group"><label for="city" class=" form-control-label">Email</label><input type="email" name="email" value="<?php echo $row->Email; ?>" class="form-control" required='true'></div>
+                                                        <div class="form-group">
+                                                            <label for="city" class="form-control-label">Email</label>
+                                                            <input type="email" name="email" value="<?php echo $row->Email; ?>" class="form-control" required='true'>
+                                                        </div>
                                                     </div>
                                                     <div class="col-12">
-                                                        <div class="form-group"><label for="postal-code" class=" form-control-label">Admin
-                                                                Registration Date</label><input type="text" name="" value="<?php echo $row->AdminRegdate; ?>" readonly="" class="form-control"></div>
+                                                        <div class="form-group">
+                                                            <label for="postal-code" class="form-control-label">Admin Registration Date</label>
+                                                            <input type="text" name="" value="<?php echo $row->AdminRegdate; ?>" readonly="" class="form-control">
+                                                        </div>
                                                     </div>
                                                 </div>
-
-                                    </div>
-                            <?php $cnt = $cnt + 1;
+                                        <?php $cnt = $cnt + 1;
                                             }
                                         } ?>
-                            <div class="card-footer">
-                                <p style="text-align: center;">
-                                    <button type="submit" class="btn btn-primary btn-sm" name="submit" id="submit">
-                                        <i class="fa fa-dot-circle-o"></i> Update
-                                    </button>
-                                </p>
+                                        <div class="card-footer">
+                                            <p style="text-align: center;">
+                                                <button type="submit" class="btn btn-primary btn-sm" name="submit" id="submit">
+                                                    <i class="fa fa-dot-circle-o"></i> Update
+                                                </button>
+                                            </p>
 
+                                        </div>
+                                    </div>
+                                </form>
                             </div>
-                            </div>
-                            </form>
+                            <!-- /.card -->
                         </div>
-                        <!-- /.card -->
+                        <!--/.col (left) -->
+                        <!-- right column -->
                     </div>
-                    <!--/.col (left) -->
-                    <!-- right column -->
-                </div>
-                <!-- /.row -->
-        </div><!-- /.container-fluid -->
-        </section>
-        <!-- /.content -->
-    </div>
-    <!-- /.content-wrapper -->
-    <?php include_once('includes/footer.php'); ?>
-    <!-- Control Sidebar -->
-    <aside class="control-sidebar control-sidebar-dark">
-        <!-- Control sidebar content goes here -->
-    </aside>
-    <!-- /.control-sidebar -->
+                    <!-- /.row -->
+                </div><!-- /.container-fluid -->
+            </section>
+            <!-- /.content -->
+        </div>
+        <!-- /.content-wrapper -->
+        <?php include_once('includes/footer.php'); ?>
+        <!-- Control Sidebar -->
+        <aside class="control-sidebar control-sidebar-dark">
+            <!-- Control sidebar content goes here -->
+        </aside>
+        <!-- /.control-sidebar -->
     </div>
     <!-- ./wrapper -->
     <!-- jQuery -->
