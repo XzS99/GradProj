20446cda32705ce637b09fd1ac14dc610a10bdbb
wesam jarrah <wesam.jarrahh@gmail.com>
2023-01-22 20:31:49 +0300

    Added some comments

--- craftsmanship/logout.php
+++ craftsmanship/logout.php
@@ -1,6 +1,18 @@
 <?php
+// Start a new session
 session_start();
+
+// Unset all session variables
 session_unset();
+
+// Reset session data
 session_reset();
+
+// Destroy the session
 session_destroy();
+
+// Redirect the user to the login page
 header('location:login.php');
+?>
+
+
